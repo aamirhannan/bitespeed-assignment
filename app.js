f2fbf3f9e56386e1af7acb47ccf6b7edb66a5157
aamirhannan <aamirhannan08@gmail.com>
2025-02-10 01:16:24 +0530

    env setup

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
+require('dotenv').config();
 const express = require('express')
-
+const bodyParser = require('body-parser');
 
 const app = express()
-const PORT = 3000
 
+const PORT = process.env.PORT || 3000
 
+// Middleware
+app.use(bodyParser.json());
 
-
-
-
-
-app.listen(() => {
+app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
